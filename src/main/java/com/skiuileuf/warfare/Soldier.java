772e6929dbe9326c4072975d0188290deed9ce76
imsky <imsky@ubuntu.ubuntu-domain>
2018-04-11 21:19:04 +0300

    Add existing file

---
+++ src/main/java/com/skiuileuf/warfare/Soldier.java
@@ -0,0 +1,14 @@
+package com.skiuileuf.warfare;
+
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.world.World;
+
+public class Soldier extends EntityCreature {
+
+	public Soldier(World p_i1602_1_) {
+		super(p_i1602_1_);
+		// TODO Auto-generated constructor stub
+		//this.maxHealth = 20f;
+	}
+
+}
---
+++ src/main/java/com/skiuileuf/warfare/WarfareMod.java
@@ -0,0 +1,25 @@
+package com.skiuileuf.warfare;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = "skwf" , name = "Fulda Warfare" , version = "1.0")
+
+public class WarfareMod {
+	@EventHandler
+	public void PreInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	public void Init(FMLInitializationEvent event)
+	{
+		
+	}
+	public void PostInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
